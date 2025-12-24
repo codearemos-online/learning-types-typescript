2d8ee7a1cd2015d570da4546bb6b70af2fe50b2f
codearemos-online <dev@codearemos.online>
2025-12-23 19:48:37 -0500

    feat(optionals) Using optionals in functions

--- src/function.ts
+++ src/function.ts
@@ -33,3 +33,27 @@ function printName(name:string):void{
 } 
 
 let namePerson = printName("Doe")
+
+
+/**
+ *
+ * OPTIONAL PARAMETERS
+ * 
+*/
+
+//Initialize values to params
+const printNameAndOptions = (name:string,options:{debugMode:boolean} = {debugMode: true}) => {
+    console.log(name,options)
+}
+
+type Options = {
+    debugMode:boolean
+}
+
+const printNameAndOptionsOpc = (name:string,options?:Options) => {
+    console.log(name,options)
+}
+
+printNameAndOptions('Testing name')
+printNameAndOptionsOpc('Testing name opc', {debugMode:false})
+
